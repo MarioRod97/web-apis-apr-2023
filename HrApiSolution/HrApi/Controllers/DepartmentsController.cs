1c94eddc8705d2a52f0e4ab24cd34a707c043e66
MarioRod97 <Mario_Rodriguez_36@progressive.com>
2023-04-17 15:16:41 -0400

    Before afternoon break push

--- HrApiSolution/HrApi/Controllers/DepartmentsController.cs
+++ HrApiSolution/HrApi/Controllers/DepartmentsController.cs
@@ -1,19 +1,44 @@
-﻿using HrApi.Models;
+﻿using HrApi.Domain;
+using HrApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HrApi.Controllers;
 
 public class DepartmentsController : ControllerBase
 {
+    private readonly HrDataContext _context;
+
+    public DepartmentsController(HrDataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("/departments")]
+    public async Task<ActionResult> AddADepartment([FromBody] DepartmentCreateRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            // return 400 error code
+            return BadRequest(ModelState);
+        }
+        
+        return Ok(request);
+    }
+
     [HttpGet("/departments")]
     public async Task<ActionResult<DepartmentResponse>> GetDepartments()
     {
         var response = new DepartmentResponse
         {
-            Data = new List<DepartmentSummaryItem> {
-                new DepartmentSummaryItem { Id="1", Name="Developers" },
-                new DepartmentSummaryItem { Id="2", Name="Testers" }
-            }
+            Data = await _context.Departments
+                                    .Select(d =>
+                                        new DepartmentSummaryItem
+                                        {
+                                            Id = d.Id.ToString(),
+                                            Name = d.Name,
+                                        })
+                                    .ToListAsync()
         };
         
         return Ok(response);
